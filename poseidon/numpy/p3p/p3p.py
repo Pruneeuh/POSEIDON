f3fd442c45733eac518ebcf64a049c61a964ec34
Prune <prune.mamalet@etu.inp-n7.fr>
2025-07-18 16:10:32 +0200

    Initialisation numpy

---
+++ poseidon/numpy/p3p/p3p.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+
+from numpy import ndarray
+from typing import Sequence
+
+
+def solve_reformat_p3p_solutions(points_3D : ndarray ,points_2D : ndarray, A : ndarray) -> ndarray:
+    """Reformat the P3P solutions from opencv to a specific structure.
+    Args:
+        points_3D (np.ndarray): 3 3D points of shape (3, 3).
+        points_2D (np.ndarray): Projected 2D points of shape (3, 2).
+        A (np.ndarray): Intrinsic camera matrix of shape (3, 3).
+    Returns:
+        solutions (np.ndarray): Reformatted solutions of shape (4, 3, 4).
+    """
+    nbsol : int 
+    rvec : Sequence[ndarray]
+    tvecs : Sequence[ndarray]
+    nbsol, rvec, tvecs =  cv2.solveP3P(points_3D,points_2D,A,np.zeros(4,), flags = cv2.SOLVEPNP_P3P)  # Solve P3P problem using OpenCV
+
+    solutions  : ndarray = np.zeros((4,3,4))     # (4*3*4)
+
+    for i in range(len(rvec)) : 
+        # Transition from the Rodriguez vectors to the rotation matrix 
+        rodriguez : ndarray = rvec[i]     # (3*1)
+        R_P3P : ndarray = cv2.Rodrigues(rodriguez)[0]    # rotation matrix : (3*3)
+
+        C_P3P : ndarray = tvecs[i]    # translation matrix : (3*1)
+
+        solutions[i,:,:1]= C_P3P
+        solutions[i,:,1:] = R_P3P
+    return solutions
---
+++ poseidon/numpy/utils/points.py
@@ -0,0 +1,35 @@
+import numpy as np
+from numpy import ndarray
+
+def generate_points_3D() -> ndarray : 
+    """Generate random 3D points in a space 4*4*4
+        points_3D (np.ndarray): Random 3D points of shape (4, 3).
+    """
+
+    return np.random.rand(4, 3)*[2.0,2.0,2.0]  # 4 points in 3D (4*3)
+
+def projection_points_2D(points_3D : ndarray, C : ndarray, R : ndarray ,A : ndarray) -> ndarray: 
+    """Project 3D points to 2D using camera parameters.
+    Args:
+        points_3D (np.ndarray): 4 3D points of shape (4, 3).
+        C (np.ndarray): Camera position vector of shape (3, 1).
+        R (np.ndarray): Camera rotation matrix of shape (3, 3).
+        A (np.ndarray): Intrinsic camera matrix of shape (3, 3).
+    Returns:
+        point2D (np.ndarray): Projected 2D points of shape (4,2).
+    """
+
+    PI : ndarray = np.concatenate((np.eye(3),np.zeros((3,1))),axis=1)  # (3*4)
+
+    Rt : ndarray = np.concatenate((R,C),axis=1)               # (3*4)
+    Rt = np.concatenate((Rt,np.array([[0,0,0,1]])),axis=0)   # (4*4)
+
+    points_2D : ndarray = np.zeros((4,2))  # Initialize the array for 2D points (4*2)
+    # Project each 3D point to 2D
+    for i in range(4) : 
+        point3D_bis : ndarray= np.concatenate((np.reshape(points_3D[i,:],(3,1)),np.array([[1]])),axis=0)   #(4*1)
+        point2D : ndarray = A @ PI @ Rt @ point3D_bis   # 2D point = [u, v, w] (3*1)
+        point2D = point2D / point2D[2]        # 2D point = [u, v, 1] (3*1)
+        points_2D[i,:] = point2D[:2].reshape(1,2)  # Store the 2D point in the array
+
+    return points_2D 
